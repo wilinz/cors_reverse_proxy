bc21fda06cbadc2aaee5ec2852b530262b7fdc91
wilinz <weiizan71@gmail.com>
2023-01-03 03:43:20 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # cors_reverse_proxy
 golang cors 反向代理，使得web端可以跨域访问任何链接
+
+运行服务端：
+```shell
+go run main.go  #运行
+go build  #构建
+```
+
+flutter dio web 客户端示例：
+```dart
+import 'package:dio/adapter_browser.dart';
+import 'package:dio/browser_imp.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+Future<void> main() async {
+      Dio dio;
+      if (kIsWeb) {
+        dio = DioForBrowser(option);
+        var adapter = BrowserHttpClientAdapter();
+        // This property will automatically set cookies
+        adapter.withCredentials = true;
+        dio.httpClientAdapter = adapter;
+      } else {
+        dio = Dio(option);
+      }
+      
+      dio.interceptors.add(WebProxyInterceptor());
+      final resp = await dio.get("https://www.baidu.com/")
+}
+     
+class WebProxyInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (kIsWeb) {
+      final newOption = options.copyWith(baseUrl: "http://127.0.0.1:9999/");
+      newOption.path = "/proxy";
+      newOption
+        ..queryParameters.clear()
+        ..queryParameters["url"] = options.uri.toString();
+      _replaceHeader(newOption, ["Referer", "User-Agent"]);
+      handler.next(newOption);
+      return;
+    }
+    handler.next(options);
+  }
+
+  void _replaceHeader(RequestOptions newOption, List<String> keys) {
+    keys.forEach((key) {
+      var referer = newOption.headers[key];
+      Logger().d(referer);
+      if (referer != null) {
+        newOption.headers["X-$key"] = referer.toString();
+        newOption.headers.remove(key);
+      }
+    });
+  }
+}
+```
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+var proxyPath = "/proxy"
+
+func main() {
+
+	httpClient := &http.Client{
+		//Transport: tr,
+		Timeout: time.Second * 30, //超时时间
+	}
+
+	r := gin.Default()
+
+	//"http://127.0.0.1:9999/proxy?url=https://www.baidu.com"
+	r.Any(proxyPath, func(c *gin.Context) {
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Referer, X-User-Agent")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.Writer.WriteHeader(200)
+			return
+		}
+		urlString := c.Query("url")
+
+		originUrl, err := parseOriginUrl(urlString)
+
+		if err != nil {
+			c.String(http.StatusBadRequest, "url参数错误")
+			return
+		}
+
+		req, _ := http.NewRequest(c.Request.Method, urlString, c.Request.Body)
+		copyRequestHeader(c, req)
+
+		resp, err := httpClient.Do(req)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "代理服务器异常")
+			return
+		}
+
+		copyResponseHeader(c, resp)
+		modifyLocation(c, originUrl.String())
+		io.Copy(c.Writer, resp.Body)
+	})
+	err := r.Run(":9999")
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+}
+
+func parseOriginUrl(urlString string) (*url.URL, error) {
+	originUrl, err := url.Parse(urlString)
+	originUrl.Path = "/"
+	originUrl.RawQuery = ""
+	return originUrl, err
+}
+
+func modifyLocation(c *gin.Context, origin string) {
+	location := c.Writer.Header().Get("Location")
+	if isAbsolutePath(location) {
+		location = buildProxyUrl(origin + location)
+	} else if isFullURL(location) {
+		location = buildProxyUrl(location)
+	}
+	c.Writer.Header().Set("Location", location)
+}
+
+func copyResponseHeader(c *gin.Context, resp *http.Response) {
+	cookies := resp.Cookies()
+	for _, cookie := range cookies {
+		cookie.SameSite = http.SameSiteNoneMode
+		cookie.Secure = true
+		c.Writer.Header().Add("Set-Cookie", cookie.String())
+	}
+	c.Writer.WriteHeader(resp.StatusCode)
+	for k, vList := range resp.Header {
+		if k != "Set-Cookie" {
+			for _, v := range vList {
+				c.Writer.Header().Add(k, v)
+			}
+		}
+	}
+}
+
+func copyRequestHeader(c *gin.Context, req *http.Request) {
+	for k, vList := range c.Request.Header {
+		newKey := k
+		if strings.HasPrefix(k, "X-") {
+			newKey = k[len("X-"):]
+			c.Request.Header.Del(newKey)
+			req.Header.Del(newKey)
+		}
+		for _, v := range vList {
+			req.Header.Add(newKey, v)
+		}
+	}
+}
+
+func buildProxyUrl(uri string) string {
+	return proxyPath + "?url=" + url.QueryEscape(uri)
+}
+func isAbsolutePath(uri string) bool {
+	return strings.HasPrefix(uri, "/")
+}
+
+func isFullURL(uri string) bool {
+	return strings.HasPrefix(uri, "http://") || strings.HasPrefix(uri, "https://")
+}
